a6eac43d5ca82bc137ff54ef2552dabd5220776e
Davit <mamulashvili1985@gmail.com>
2026-03-10 01:38:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "client-totals-v16";
+const CACHE = "client-totals-v17";
 
 const ASSETS = [
   "./",
@@ -32,6 +32,7 @@ self.addEventListener("fetch", (event) => {
   );
 
 });
+
 
 
 
