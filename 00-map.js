1935ed78502bc997c65bae7c95d2df62bd0bef1f
Davit <mamulashvili1985@gmail.com>
2026-04-07 21:43:02 +0200

    Add files via upload

---
+++ 00-map.js
@@ -0,0 +1,71 @@
+# JS MAP (functions)
+
+01-config.js
+- initConfig()
+- DEFAULTS
+- STORAGE_KEYS
+
+02-dom.js
+- getElementById refs
+- overviewDateRangeEl
+- controlsToggle
+- groupPickerBtn
+
+03-utils.js
+- formatDateForRange()
+- formatDateLocal()
+- parseMoney()
+- fmt()
+- getDurationMonthsDays()
+- getGroupsDateRange()
+
+04-state.js
+- appState
+- activeGroup()
+- getGroupsByMode()
+- setMode()
+
+05-storage.js
+- saveState()
+- loadState()
+
+06-render.js
+- render()
+- renderOverviewDateRange()
+- renderMonthlyStats()
+- renderReview()
+- recalcAndRenderTotals()
+- updateControlsButtonLabel()
+
+07-modals.js
+- askConfirm()
+- openModal()
+- closeModal()
+
+08-groups.js
+- addGroup()
+- renameGroup()
+- deleteGroup()
+- archiveGroup()
+
+09-periods.js
+- addPeriod()
+- removePeriod()
+- validatePeriodWarnings()
+
+10-search.js
+- initReviewSearch()
+- renderSearchResults()
+
+11-import-export.js
+- exportJSON()
+- importJSON()
+- importExcel()
+
+12-theme.js
+- setTheme()
+- toggleTheme()
+
+13-app.js
+- initApp()
+- startApp()
